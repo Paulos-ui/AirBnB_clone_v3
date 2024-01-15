870b7f29a29ca386acb0cde1395c156691e64fbd
root <tunmisejayeoba@gmail.com>
2024-01-15 14:31:24 -0800

    app

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Module: app """
+
+from flask_cors import CORS
+from flask import Flask, jsonify
+from os import getenv
+from api.v1.views import app_views
+from models import storage
+
+app = Flask(__name__)
+
+# Task 12
+CORS(app, resources={r"/api/v1/*": {"origins": "0"}})
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Removes the current session obj after each request """
+    storage.close()
+
+
+# Task 5
+@app.errorhandler(404)
+def not_found(error):
+    """ This returns JSON response with 'Not found' error message"""
+    resp = {'error': 'Not found'}
+    return jsonify(resp), 404
+
+
+if __name__ == "__main__":
+    HOST = getenv("HBNB_API_HOST", "0")
+    PORT = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
